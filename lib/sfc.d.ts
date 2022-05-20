d5a8bb2c37d89855542b6574975e1b8b95937fb2
Ryan Brereton <ryan@luminsports.com>
2022-05-20 15:29:29 +0930

    feat: add vite build scripts

--- lib/sfc.d.ts
+++ lib/sfc.d.ts
@@ -1,4 +1,8 @@
+/// <reference types="vue/ref-macros" />
+
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import typescript from '@rollup/plugin-typescript'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  resolve: {
+    alias: {
+    },
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      name: 'index',
+      fileName: format => `index.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: { vue: 'vue' },
+      },
+      plugins: [
+        typescript({
+          target: 'es2020',
+          rootDir: path.resolve('lib'),
+          declaration: true,
+          declarationDir: path.resolve('dist'),
+          exclude: path.resolve('node_modules/**'),
+          allowSyntheticDefaultImports: true
+        })
+      ]
+    }
+  },
+})
