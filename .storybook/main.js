2aa185637ef68d93fd240afb96768a499b5c4999
Lucas M <lmartin@luminsports.com>
2022-07-08 15:46:06 +0930

    feat: tailwind in stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,15 @@
 module.exports = {
-  "stories": [
-    "../lib/**/*.stories.mdx",
-    "../lib/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../lib/**/*.stories.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import './app.css'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
