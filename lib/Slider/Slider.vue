5d5c3f2ae623361cb2275c74bef3968de85be1c9
Ryan Brereton <ryan@luminsports.com>
2022-07-08 16:07:10 +0930

    feat: providing inject position to more of the slider subcomponents

--- lib/Slider/Slider.vue
+++ lib/Slider/Slider.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-  import { provide } from 'vue'
+  import { provide, reactive } from 'vue'
 
   const props = defineProps({
     step: { type: Number, default: 1 },
@@ -7,9 +7,19 @@
     max: { type: Number, default: 100 },
   })
 
+  const thumbs = reactive({})
+
   provide('step', props.step)
   provide('min', props.min)
   provide('max', props.max)
+  provide('thumbs', thumbs)
+  provide('registerThumb', (thumb, value) => {
+    thumbs[thumb] = value
+
+    return () => {
+      delete thumbs[thumb]
+    }
+  })
 </script>
 
 <template>
--- lib/Slider/SliderThumb.vue
+++ lib/Slider/SliderThumb.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, inject, getCurrentInstance, onMounted, onUnmounted, computed } from 'vue'
+  import { linearInterpolation } from './utils'
 
   const props = defineProps({
     preventOverlap: {
@@ -15,12 +16,32 @@
     },
   })
 
+
+  const registerThumb = inject('registerThumb')
+  const min = inject<number>('min')
+  const max = inject<number>('max')
+
+  let unregistered = null
+
   const hovered = ref(false)
   const pressed = ref(false)
+  const position = computed(() => {
+    const interpolatedValue = linearInterpolation(props.modelValue, min, max, 0, 100)
+
+    return { position: 'absolute', top: 0, left: `${interpolatedValue}%`}
+  })
+
+  onMounted(() => {
+    unregistered = registerThumb(getCurrentInstance().uid, props.modelValue)
+  })
+
+  onUnmounted(() => {
+    if (unregistered) {
+      unregistered()
+    }
+  })
 </script>
 
 <template>
-  <div>
-    <slot :hovered="hovered" :pressed="pressed" />
-  </div>
+  <slot :hovered="hovered" :pressed="pressed" :pos="position" />
 </template>
