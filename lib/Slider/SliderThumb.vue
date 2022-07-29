a1e2b6415160d4d74b07097525b42d0a08154326
Ryan Brereton <ryan@luminsports.com>
2022-07-29 15:24:31 +0930

    feat: default listeners setup

--- lib/Slider/SliderThumb.vue
+++ lib/Slider/SliderThumb.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-  import { ref, getCurrentInstance, onMounted, onUnmounted, computed } from 'vue'
+  import { ref, getCurrentInstance, onMounted, onUnmounted, computed, Ref } from 'vue'
   import { linearInterpolation, useSlider } from './utils'
 
   const props = defineProps({
@@ -20,15 +20,41 @@
 
   registerThumb(getCurrentInstance().uid, ref(props.modelValue))
 
+  const element: Ref<Element> = ref(null)
+
+  const onMouseDown = () => {
+
+  }
+
+  const onMouseMove = () => {
+
+  }
+
+  const onMouseUp = () => {
+
+  }
+
+  onMounted(() => {
+    element.value?.addEventListener('pointerdown', onMouseDown)
+    document.addEventListener('pointermove', onMouseMove)
+    document.addEventListener('pointerup', onMouseUp)
+  })
+
+  onUnmounted(() => {
+    element.value?.removeEventListener('pointerdown', onMouseDown)
+    document.removeEventListener('pointermove', onMouseMove)
+    document.removeEventListener('pointerup', onMouseUp)
+  })
+
   const hovered = ref(false)
   const pressed = ref(false)
   const style = computed(() => {
     const interpolatedValue = linearInterpolation(props.modelValue, min, max, 0, 100)
 
-    return { position: 'absolute', top: 0, left: `${interpolatedValue}%`}
+    return { position: 'absolute', top: 0, left: `${interpolatedValue}%` }
   })
 </script>
 
 <template>
-  <slot :hovered="hovered" :pressed="pressed" :style="style" :value="props.modelValue" />
+  <slot ref='element' :hovered='hovered' :pressed='pressed' :style='style' :value='props.modelValue' />
 </template>
