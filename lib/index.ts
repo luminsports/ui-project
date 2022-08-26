3546d236fa4c0ab1d437fdffe157bb0ffc5c2881
Ryan Brereton <ryan@luminsports.com>
2022-08-26 15:15:45 +0930

    chore: delete button

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,8 @@
-import Button from './Button/Button.vue'
+import Slider from './Slider/Slider.vue'
+import SliderLabels from './Slider/SliderLabels.vue'
+import SliderRail from './Slider/SliderRail.vue'
+import SliderThumb from './Slider/SliderThumb.vue'
+import SliderTicks from './Slider/SliderTicks.vue'
+import SliderTrack from './Slider/SliderTrack.vue'
 
-export { Button }
+export { Slider }
