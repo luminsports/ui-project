7c4e8b6f43bbdf1fec6bd37cefa48359dc18baf5
Lucas M <lmartin@luminsports.com>
2022-09-09 16:18:29 +0930

    test: start of testing for slider component

--- lib/Slider/Slider.test.ts
+++ lib/Slider/Slider.test.ts
@@ -1,12 +1,36 @@
 import { mount } from '@vue/test-utils'
-import { expect, test } from 'vitest'
+import { expect, vi, it, describe } from 'vitest'
 import Slider from './Slider.vue'
+import { defineComponent } from 'vue'
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}))
+
+const slider = defineComponent({
+  name: 'test-slider',
+  components: { Slider },
+  template: `
+    <Slider>
+      <div>Nope</div>
+    </Slider>
+`,
+})
 
 /** @vitest-environment jsdom */
-test('mount slider', function() {
-  expect(Slider).toBeTruthy()
+describe('Slider:', () => {
+  it('can have a random element inside the slider for no reason!', function () {
+    expect(Slider).toBeTruthy()
 
-  const wrapper = mount(Slider, {
-    props: {},
+    const wrapper = mount(slider, {
+      props: {},
+    })
+
+    expect(wrapper.text()).toContain('Nope')
+
+    // this might not be exactly what we want to assert observe was called
+    expect(global.ResizeObserver).toHaveBeenCalled()
   })
 })
